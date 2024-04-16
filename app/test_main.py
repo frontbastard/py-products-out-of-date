a465618d0ef273e69b576fb7ca6b1bb69fa8e865
Pavlo <pavlo@MacBook-Pro-de-Pavlo.local>
2024-04-16 15:41:02 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,41 @@
-# write your code here
+import unittest.mock
+
+import pytest
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "products_list, expected",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": "2022-02-13",
+                    "price": 600
+                },
+                {
+                    "name": "sanjak",
+                    "expiration_date": "2022-02-10",
+                    "price": 600
+                }
+            ],
+            ["sanjak"],
+        ),
+
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": "2022-02-11",
+                    "price": 600
+                },
+            ],
+            []
+        )
+    ]
+)
+@unittest.mock.patch("app.main.datetime.date")
+def test_outdated_products(mock_date, products_list, expected) -> None:
+    mock_date.today.return_value = "2022-02-11"
+    assert outdated_products(products_list) == expected
